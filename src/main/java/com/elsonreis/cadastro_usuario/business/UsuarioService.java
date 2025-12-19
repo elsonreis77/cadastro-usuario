f55b2ac83901dc035624c4eb2f8857339d5e15ef
elsonreis77 <elsonreis084@gmail.com>
2025-12-19 20:09:29 -0300

    projeto completo

--- src/main/java/com/elsonreis/cadastro_usuario/business/UsuarioService.java
+++ src/main/java/com/elsonreis/cadastro_usuario/business/UsuarioService.java
@@ -30,16 +30,9 @@ public class UsuarioService {
     }
 
     public void atualizarUsuarioPorEmail(Integer id, Usuario usuario) {
-        Usuario usuarioEntity = repository.findById(id).orElseThrow(() ->
-                new RuntimeException("Usuario não encontrado"));
-        Usuario usuarioAtualizado = Usuario.builder()
-                .email(usuario.getEmail() != null ?
-                        usuario.getEmail() : usuarioEntity.getEmail())
-                .nome(usuario.getNome() != null ? usuario.getNome() :
-                        usuarioEntity.getNome())
-                .id(usuarioEntity.getId())
-                .build();
 
-        repository.saveAndFlush(usuarioAtualizado);
+    }
+
+    public void atualizarUsuarioPorId(Integer id, Usuario usuario) {
     }
 }
